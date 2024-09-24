8b04dd5b13a9e718da6e78073c99106304985e4f
Brady-Conn <jbc5099@gmail.com>
2024-09-24 09:31:52 -0600

    db testing and cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { db } from "~/server/db";
 
 const mockURLs = [
   "https://utfs.io/f/0vbOIlzL4S2APlz1OsXc5Yy3KjwmLvxGbhNsr6Ig1iF0o8zU",
@@ -11,10 +12,17 @@ const mockImages = mockURLs.map((url, index) => ({
   url
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-wrap items-center justify-center gap-12 px-4 py-16">
+        {posts.map(post => (
+          <div key={post.id} className="w-48">
+            {post.name}
+          </div>
+        ))}
         {mockImages.map(image => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="front rage scenery"/>
